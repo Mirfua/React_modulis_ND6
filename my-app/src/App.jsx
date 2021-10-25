4596c77142732d170a18c9ec2f198b3ff5e26edc
Jevgenijus <j.suchodolskis@gmail.com>
2021-10-25 11:07:02 +0300

    more simple variant update

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import FieldAnimal from "./Components/FieldAnimal";
 import idGenerator from "./Common/idGenerator";
 
-
 function App() {
 
     const [field, setField] = useState([]);
@@ -13,6 +12,7 @@ function App() {
         fieldCopy.push({
             id: idGenerator(),
             animal: what,
+            weight: 0,
             field: parseInt(fieldNamber)
         });
         setField(fieldCopy);
@@ -27,6 +27,14 @@ function App() {
         const fieldCopy = field.slice();
         const ind = fieldCopy.findIndex(e => e.id === id);
         fieldCopy.splice(ind, 1);
+        setField(fieldCopy);
+        localStorage.setItem('animals', JSON.stringify(fieldCopy))
+    }
+
+    const addWeight = (id, w) => {
+        const fieldCopy = field.slice();
+        const i = fieldCopy.findIndex(e => e.id === id);
+        fieldCopy[i].weight = parseFloat(w);
         setField(fieldCopy);
         localStorage.setItem('animals', JSON.stringify(fieldCopy))
     }
@@ -48,50 +56,21 @@ function App() {
         const animalsFromStorage = localStorage.getItem('animals');
         if (null !== animalsFromStorage) {
             setField(JSON.parse(animalsFromStorage));
+            console.log(field)
         }
     }, []);
-
-    const setWeight = (e, id, svoris) => {
-        const fieldCopy = field.slice();
-    
-        e.preventDefault();
-        console.log(fieldCopy);
-
-        let weight = svoris;
-        let curr = fieldCopy.find((el) => el.id === id);
-        let test = { ...curr, weight };
-        const ind = fieldCopy.findIndex((e) => e.id === id);
-        
-        fieldCopy.splice(ind, 1, test);
-        setField(fieldCopy);
-        localStorage.setItem("animals", JSON.stringify(fieldCopy));
-      };
-    
-      const stats = () => {
-        const fieldCopy = field.slice();
-        
-        console.log(fieldCopy);
-        
-        fieldCopy.forEach((element) => {
-          console.log(element);
-          if (element.animal === "cow") {
-            console.log(element.animal);
-          }
-        });
-      };
-      stats();
 
     return (
         <>
             <div className="field">
                 <div className="field__part">
-                    {field.map((fieldAnimal, i) => <FieldAnimal key={i} field={1} fieldAnimal={fieldAnimal} goHome={goHome} setWeight={setWeight}></FieldAnimal>)}
+                    {field.map((fieldAnimal, i) => <FieldAnimal key={i} field={1} fieldAnimal={fieldAnimal} goHome={goHome} addWeight={addWeight}></FieldAnimal>)}
                 </div>
                 <div className="field__part">
-                    {field.map((fieldAnimal, i) => <FieldAnimal key={i} field={2} fieldAnimal={fieldAnimal} goHome={goHome} setWeight={setWeight}></FieldAnimal>)}
+                    {field.map((fieldAnimal, i) => <FieldAnimal key={i} field={2} fieldAnimal={fieldAnimal} goHome={goHome} addWeight={addWeight}></FieldAnimal>)}
                 </div>
                 <div className="field__part">
-                    {field.map((fieldAnimal, i) => <FieldAnimal key={i} field={3} fieldAnimal={fieldAnimal} goHome={goHome} setWeight={setWeight}></FieldAnimal>)}
+                    {field.map((fieldAnimal, i) => <FieldAnimal key={i} field={3} fieldAnimal={fieldAnimal} goHome={goHome} addWeight={addWeight}></FieldAnimal>)}
                 </div>
             </div>
             <div className="buttons-holder">
@@ -103,12 +82,11 @@ function App() {
                     <option value={2}>Field Two</option>
                     <option value={3}>Field Three</option>
                 </select>
-
             </div>
             <div className="buttons-holder">
-                <button onClick={() => groupGoHome('cow')}>Kill cows</button>
-                <button onClick={() => groupGoHome('sheep')}>Kill sheeps</button>
-                <button onClick={() => groupGoHome('horse')}>Kill horses</button>
+                <button onClick={() => groupGoHome('cow')}>Go cows</button>
+                <button onClick={() => groupGoHome('sheep')}>Go sheeps</button>
+                <button onClick={() => groupGoHome('horse')}>Go horses</button>
             </div>
         </>
     );
@@ -116,20 +94,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
-
-// ND: 
-//  kiekvienas galvijas privalo turėti po laukelį svorio įvedimui ir mygtuką, kurį paspaudus
-//  tas svoris būtų priskiriamas galvijui. 
-
-//  Pradinis svoris yra 0 kg. Svoris kiekviename galvijaus atvaizde turi būti aiškai matomas.
-//  Svoris didesnis nei 100 turi būti atvaizduojamas kitaip (raudonai) nei mažesnis svoris. 
-
-
-//  Bonus Extra Plus: Jeigu gyvulys viršija 200 svorio jis automatiškais go home, Viršuje 
-//  statistikos komponentas skaičiuoja visus gyvulius bendrai, kiekvieną rūšį atskirai 
-//  ir bendrą visos bandos svorį.
--- my-app/src/Components/FieldAnimal.jsx
+++ my-app/src/Components/FieldAnimal.jsx
@@ -1,51 +1,26 @@
-import { useState } from "react";
+import OneAnimal from "./OneAnimal";
 
-function FieldAnimal({ fieldAnimal, field, goHome, setWeight }) {
-    const [input, setInput] = useState();
+function FieldAnimal({ fieldAnimal, field, goHome, addWeight }) {
 
-    const Change = (e) => {
-      setInput(e.target.value);
-    };
-    if (fieldAnimal.weight > 200) {
-      goHome(fieldAnimal.id);
-    }
 
     if (field === fieldAnimal.field && 'cow' === fieldAnimal.animal) {
         return (
             <div className="cow">
-                <form className="div" onSubmit={(e) => setWeight(e, fieldAnimal.id, input)}>
-                    <button type="submit">Set weight:</button>
-                    <input onChange={(e) => Change(e)} className="input" type="text"/>
-                </form>
-                <h2>{fieldAnimal.id}</h2>
-                <span style={{ color: fieldAnimal.weight > 100 ? "red" : "black" }}>Svoris: {fieldAnimal.weight}</span>                
-                <button onClick={() => goHome(fieldAnimal.id)}>Kill</button>
+                <OneAnimal goHome={goHome} addWeight={addWeight} fieldAnimal={fieldAnimal} ></OneAnimal>
             </div>
         );
     }
     else if (field === fieldAnimal.field && 'sheep' === fieldAnimal.animal) {
         return (
             <div className="sheep">
-                <form className="div" onSubmit={(e) => setWeight(e, fieldAnimal.id, input)}>
-                    <button type="submit">Set weight:</button>
-                    <input onChange={(e) => Change(e)}className="input" type="text"/>
-                </form>
-                <h2>{fieldAnimal.id}</h2>
-                <span style={{ color: fieldAnimal.weight > 100 ? "red" : "black" }}>Svoris: {fieldAnimal.weight}</span>                
-                <button onClick={() => goHome(fieldAnimal.id)}>Kill</button>
+                <OneAnimal goHome={goHome} addWeight={addWeight} fieldAnimal={fieldAnimal} ></OneAnimal>
             </div>
         );
     }
     else if (field === fieldAnimal.field && 'horse' === fieldAnimal.animal) {
         return (
             <div className="horse">
-                <form className="div" onSubmit={(e) => setWeight(e, fieldAnimal.id, input)}>
-                    <button type="submit">Set weight:</button>
-                    <input onChange={(e) => Change(e)} className="input" type="text"/>
-                </form>
-                <h2>{fieldAnimal.id}</h2>
-                <span style={{ color: fieldAnimal.weight > 100 ? "red" : "black" }}>Svoris: {fieldAnimal.weight}</span>                
-                <button onClick={() => goHome(fieldAnimal.id)}>Kill</button>
+                <OneAnimal goHome={goHome} addWeight={addWeight} fieldAnimal={fieldAnimal} ></OneAnimal>
             </div>
         );
     }
